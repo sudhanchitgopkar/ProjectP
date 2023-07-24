e41a88b139edddb4fff4614d63fbeb1ed04a9923
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-07-23 22:03:16 -0400

    🎉 solved 50

---
+++ leetcode/50.Pow/sc_sol.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double sol = pls(x, n);
+        return n < 0 ? 1.0/sol : sol;
+    } //myPow
+    
+    double pls(double x, int n) {
+        if (n == 0) return 1.0;
+
+        double res = pls(x, n/2);
+        res *= res;
+        if (n % 2 == 0) return res;
+        return res * x;
+    } //pow
+};
