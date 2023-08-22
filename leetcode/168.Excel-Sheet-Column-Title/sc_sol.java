06ce8fba8137f9feb32158f174c94e085b75b343
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-08-21 22:18:29 -0400

    🎉 solved 168

---
+++ leetcode/168.Excel-Sheet-Column-Title/sc_sol.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int num) {
+        StringBuilder sol = new StringBuilder();
+        
+        while (--num >= 0) {
+            sol.insert(0, (char)((num % 26) + 'A'));
+            num /= 26;
+        } //while
+
+        return sol.toString();
+    } //convert
+} //Sol
