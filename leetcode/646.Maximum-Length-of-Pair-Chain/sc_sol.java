0a1ef56f3513d38fdceaefb6406fa09b581c4d16
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-08-25 22:18:35 -0400

    🎉 solved 646

---
+++ leetcode/646.Maximum-Length-of-Pair-Chain/sc_sol.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.<int []>sort(pairs, (a, b) -> (Integer.compare(a[1], b[1])));
+        int sol = 1, end = pairs[0][1];
+
+        for (int i = 1; i < pairs.length; i++) {
+            if (pairs[i][0] > end) {
+                ++sol;
+                end = pairs[i][1];
+            } //if
+        } //for
+        
+        return sol;
+    } //findLongest
+} //Sol
