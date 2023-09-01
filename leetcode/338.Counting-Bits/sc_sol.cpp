29ea15331cb5d3eb195a113af9c1793537cb7143
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-08-31 23:33:32 -0400

    🎉 solved 338

---
+++ leetcode/338.Counting-Bits/sc_sol.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> sol {0};
+        for (int i = 1; i <= n; i++)
+            sol.push_back(sol[i/2] + i % 2);
+        return sol;
+    } //countingBits
+};
