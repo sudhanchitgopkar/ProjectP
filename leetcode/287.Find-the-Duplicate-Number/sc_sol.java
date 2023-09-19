4110307241fcf6f07d16f0e40cfc04f796370ad2
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-09-19 09:55:57 -0400

    🎉solved 287

---
+++ leetcode/287.Find-the-Duplicate-Number/sc_sol.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int val = 0;
+
+        for (int n : nums) {
+          
+        } //for
+
+        return -1; //unreachable
+    } //findDup
+} //Sol
